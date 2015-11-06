a333f1faddb8a463e050dde329964a0c338da7e7
Trevor Hutto <huttotw@gmail.com>
2015-11-05 22:00:40 -0500

    added master password and qr code

--- js/options.js
+++ js/options.js
@@ -1,33 +1,21 @@
 // Save the options to local stroage
 function save_options(e) {
     e.preventDefault();
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
+    var masterPassword = document.getElementById("masterPassword").value;
 
-    chrome.storage.sync.set({
-        email: email,
-        phone: phone
-    }, function () {
+    var key = "masterPassword";
+    var value = masterPassword;
+    var json = {};
+    json[key] = value;
+
+    chrome.storage.sync.set(json, function () {
         var status = document.getElementById("status");
+        var oldStatus = status.textContent;
         status.textContent = "Options saved!";
         setTimeout(function () {
-            status.textContent = "";
+            status.textContent = oldStatus;
         }, 2000);
     });
 };
 
-// Retrieve the options
-function restore_options(e) {
-    e.preventDefault();
-    chrome.storage.sync.get({
-        email: "", // default
-        phone: "" // default
-    }, function (items) {
-        document.getElementById("email").value = items.email;
-        document.getElementById("phone").value = items.phone;
-    });
-}
-
-// Add listeners for the buttons and onLoad
-document.addEventListener("DOMContentLoaded", restore_options);
 document.getElementById("save").addEventListener("click", save_options);
--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,30 @@
  * to a string, creating a length 32 substring, then hashing it
  * with SHA512.
 **/
+function generateBarCode (e) {
+    e.preventDefault();
+    var qrcode = document.getElementById("qrcode");
+    var submittedPasswordNode = document.getElementById("masterPassword");
+    var submittedPassword = submittedPasswordNode.value;
+    chrome.storage.sync.get("masterPassword", function (items) {
+        if (items.masterPassword === submittedPassword) {
+            // Clear the password;
+            submittedPasswordNode.value = "";
+            qrcode.style.display = "block";
+
+            // Generate pin
+            var code = new QRCode(document.getElementById("qrcode"), "Like a boss");
+        } else {
+            qrcode.style.display = "none";
+        }
+    });
+};
+
+/***************************************************************
+ * Generate a password by generating a random number, changing
+ * to a string, creating a length 32 substring, then hashing it
+ * with SHA512.
+**/
 function generatePassword () {
     var number = Math.random() * 9999999999;
     var binary = number.toString(2);
@@ -10,6 +34,23 @@ function generatePassword () {
     document.getElementById("generatedPassword").textContent = generatedPassword;
 };
 
+/***************************************************************
+ * Decide what part of the pop up we need to show.
+**/
+function manipulatePopup () {
+    chrome.tabs.query({'active': true}, function (tabs) {
+        var url = tabs[0].url;
+        var regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/;
+        var domain = url.replace(regex, "$1");
+        chrome.storage.sync.get(domain, function (items) {
+            if (items[domain]) {
+                document.getElementById("create").style.display = "none";
+            } else {
+                document.getElementById("retrieve").style.display = "none";
+            }
+        });
+    });
+};
 /***************************************************************
  * We save the password to the local storage for later
  * retrieval. It should be encrypted, and the encryption
@@ -20,16 +61,17 @@ function save () {
     var generatedPassword = document.getElementById("generatedPassword");
     chrome.tabs.query({'active': true}, function (tabs) {
         var url = tabs[0].url;
-        console.log(url);
         var regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/;
         var domain = url.replace(regex, "$1");
 
+        // Set the key value pair to be inserted.
+        var key = domain;
+        var value = generatedPassword.textContent;
+        var json = {};
+        json[key] = value;
+
         // Save to storage
-        chrome.storage.sync.set({
-            url: url,
-            password: generatedPassword
-        }, function () {
-            
+        chrome.storage.sync.set(json, function () {
             // Select the password and prepare to copy
             var range = document.createRange();
             range.selectNode(generatedPassword);
@@ -58,6 +100,10 @@ function save () {
 // Generate it the first time
 generatePassword();
 
+// Decide what we want to show
+manipulatePopup();
+
 // Add listeners for our buttons
 document.getElementById("save").addEventListener("click", save);
 document.getElementById("regenerate").addEventListener("click", generatePassword);
+document.getElementById("submitMasterPassword").addEventListener("click", generateBarCode);
